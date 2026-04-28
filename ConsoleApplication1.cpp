ee0a40f8f2164da5c0ca4bb5d36f39a6d60017c7
vipda <vipda@DESKTOP-MR1E4N4>
2026-04-29 00:40:45 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,119 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+#include <locale.h>
+
+#define N 1024
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    static double A[N][N], B[N][N], C[N][N];
+    int i, j, k, r, var, t;
+    int nt;
+    double start, end, min_time;
+    double t1_ikj = 0.0, t1_kij = 0.0;
+
+    srand(time(NULL));
+
+    for (i = 0; i < N; i++)
+        for (j = 0; j < N; j++)
+        {
+            A[i][j] = (double)rand() / RAND_MAX;
+            B[i][j] = (double)rand() / RAND_MAX;
+        }
+
+    printf("Умножение матриц %dx%d\n", N, N);
+    printf("Порядок   | Потоков |   Время (с)   | Ускорение | Эффективность\n\n");
+
+    int threads[] = { 1, 2, 4, 8, 12, 16, 20, 24, 28, 32, 34 };
+
+    for (t = 0; t < 11; t++)
+    {
+        nt = threads[t];
+        omp_set_num_threads(nt);
+
+        for (var = 0; var < 2; var++)
+        {
+            const char* order;
+            if (var == 0) {
+                order = "i-k-j";
+            }
+            else {
+                order = "k-i-j";
+            }
+
+            min_time = 1e9;
+
+            for (r = 0; r < 3; r++){
+                for (i = 0; i < N; i++) {
+                    for (j = 0; j < N; j++) {
+                        C[i][j] = 0.0;
+                    }
+                }
+
+                start = omp_get_wtime();
+
+                #pragma omp parallel
+                {
+                    if (var == 0) {
+                        #pragma omp for schedule(dynamic)
+                        for (i = 0; i < N; i++) {
+                            for (k = 0; k < N; k++) {
+                                for (j = 0; j < N; j++) {
+                                    C[i][j] += A[i][k] * B[k][j];
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        #pragma omp for schedule(dynamic)
+                        for (k = 0; k < N; k++) {
+                            for (i = 0; i < N; i++) {
+                                for (j = 0; j < N; j++) {
+                                    C[i][j] += A[i][k] * B[k][j];
+                                }
+                            }
+                        }
+                    }
+                }
+
+                end = omp_get_wtime();
+
+                if (end - start < min_time) {
+                    min_time = end - start;
+                }
+            }
+
+            if (nt == 1) {
+                if (var == 0) {
+                    t1_ikj = min_time;
+                }
+                else {
+                    t1_kij = min_time;
+                }
+            }
+
+            double speedup;
+            if (nt == 1) {
+                speedup = 1.0;
+            }
+            else {
+                if (var == 0) {
+                    speedup = t1_ikj / min_time;
+                }
+                else {
+                    speedup = t1_kij / min_time;
+                }
+            }
+
+            double eff = speedup / nt * 100.0;
+
+            printf("%-8s  | %2d      | %.6f      | %.6f  | %.6f%%\n",
+                order, nt, min_time, speedup, eff);
+        }
+    }
+
+    return 0;
+}
